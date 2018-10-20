c8c5793613168e3459795b2086938979bea5b7aa
anurag.v70 <anurag.v70@wipro.com>
2018-10-21 00:31:26 +0530

    sbilinks

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const clickedUsers = require("./uniqueID");
+
+mongoose.connect("mongodb://anuragvasi:anurag61@ds237373.mlab.com:37373/sbiintrestedusers",{ useNewUrlParser: true }, 
+    (err,data) => {
+    if(err){
+        console.log("error connecting mongoDB", err);
+    }
+    else{
+        console.log("sucessfully connecte dto DB");
+    }
+})
+
+app.get("/", (req,res) => {
+    res.redirect("https://www.onlinesbi.com/");
+})
+
+app.get("/:customerid", (req,res,next) => {
+    const customerid = req.params.customerid;
+    console.log("",customerid);
+    const user = new clickedUsers({
+        clickedID:customerid
+    });
+
+    user.save((err,data) => {
+        if(err){
+            console.log('error saving to db', customerid);
+        }
+        if(data){
+            console.log('saved to DB');
+        }
+        res.redirect("https://www.onlinesbi.com/");
+    });
+
+    
+});
+
+
+app.listen(3000, () => {
+    console.log("server running at port 3000");
+})
